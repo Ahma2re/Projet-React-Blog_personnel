2c84e0423a45023302debded4a9c918157b3dfe1
Ahmady <toureahmady012@gmail.com>
2026-04-10 20:47:08 +0000

    Modification url pour deployer

--- blog_personnel/src/services/api.js
+++ blog_personnel/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // ─── Configuration Axios ──────────────────────────────────────────────────────
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://projet-react-blog-personnel.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
